07f3ee601a8bdc103559d9cfe1e3b001f4ec6f22
ihorbenko <ihorbenko@plexteq.com>
2020-08-01 12:44:10 +0300

    Used experimental GStreamer installation

--- teevid-client-native/install.sh
+++ teevid-client-native/install.sh
@@ -1,127 +1,37 @@
 #!/bin/sh
 
-ROOT_DIR=$(pwd)
-ARCHITECTURE=$(uname -m)
-INSTALL_PREFIX=/usr
-INSTALL_LIB_DIR=/usr/lib/$ARCHITECTURE-linux-gnu
-if [ "$ARCHITECTURE" = "aarch64" ]; then
-  #run the fan
-  sudo jetson_clocks
-fi
-
-#gstreamer
-sudo apt-get install --yes build-essential dpkg-dev flex bison autotools-dev automake liborc-dev autopoint libtool gtk-doc-tools libgstreamer1.0-dev
-sudo apt install --yes libnice-dev gstreamer1.0-nice libssl-dev
-
-#gstreamer plugins-base
-sudo apt-get install --yes libxv-dev libasound2-dev libtheora-dev libogg-dev libvorbis-dev
-
-#gstreamer plugins-good
-sudo apt-get install --yes libbz2-dev libv4l-dev libvpx-dev libjack-jackd2-dev libsoup2.4-dev libpulse-dev
-
-#gstreamer plugins-bad
-sudo apt-get install --yes faad libfaad-dev libfaac-dev
-
-#gstreamer plugins-ugly
-sudo apt-get install --yes libx264-dev libmad0-dev
-
-install_libnice() {
-  echo "Install libnice"
-  #libnice
-  sudo apt install --yes python3-pip
-  pip3 install meson
-  PATH=~/.local/bin:$PATH
-  sudo apt-get install libgnutls28-dev --yes
-  sudo apt-get install ninja-build --yes
-  git clone https://github.com/TeeVid/libnice.git
-  cd libnice
-  meson --prefix=$INSTALL_PREFIX build && ninja -C build && sudo ninja -C build install
-  cd $ROOT_DIR
-  echo "libnice installed"
-}
-
-install_libnice
-
-if [  "$ARCHITECTURE" = "aarch64" ]; then
-  echo "Install GStreamer with 'gst-install'"
-  sudo gst-install --prefix=$INSTALL_PREFIX --version=1.16.2
-# temporary fix for missed vp8/vp9 keyframe
-  sudo cp ./3rd_party_libs/embedded/libgstrtp-1.0.so.0.1602.0 $INSTALL_LIB_DIR
-# temporary fix for 'gst_buffer_resize_range: assertion 'bufmax >= bufoffs + offset + size' failed'
-  sudo cp ./3rd_party_libs/embedded/libgstreamer-1.0.so.0.1602.0 $INSTALL_LIB_DIR
-else
-  echo "Install GStreamer manually"
-  ROOT_GST_DIR=$ROOT_DIR/gst-1.16
-  mkdir $ROOT_GST_DIR
-  cd $ROOT_GST_DIR
-
-  git clone https://github.com/GStreamer/gstreamer.git
-  cd gstreamer
-  git checkout 1.16
-  git submodule init
-  git submodule update
-  libtoolize
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  make
-  sudo make install
-  export LD_LIBRARY_PATH=$INSTALL_LIB_DIR
-  export PKG_CONFIG_PATH=$INSTALL_LIB_DIR/pkgconfig:$PKG_CONFIG_PATH
-  cd $ROOT_GST_DIR
-
-  git clone https://github.com/GStreamer/gst-plugins-base.git
-  cd gst-plugins-base
-  git checkout 1.16
-  git submodule init
-  git submodule update
-  libtoolize
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  make
-  sudo make install
-  cd $ROOT_GST_DIR
-
-  git clone https://github.com/GStreamer/gst-plugins-good.git
-  cd gst-plugins-good
-  git checkout 1.16
-  git submodule init
-  git submodule update
-  libtoolize
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  make
-  sudo make install
-  cd ..
-
-  git clone https://github.com/GStreamer/gst-plugins-bad.git
-  cd gst-plugins-bad
-  git checkout 1.16
-  git submodule init
-  git submodule update
-  libtoolize
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  make
-  sudo make install
-  cd ..
-
-  git clone https://github.com/GStreamer/gst-plugins-ugly.git
-  cd gst-plugins-ugly
-  git checkout 1.16
-  git submodule init
-  git submodule update
-  libtoolize
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  ./autogen.sh --prefix=$INSTALL_PREFIX --libdir=$INSTALL_LIB_DIR
-  make
-  sudo make install
-  cd ..
-
-  cd $ROOT_DIR
-fi
-
-#install libnice one more time to be sure we use exactly this version
-cd libnice
-sudo ninja -C build install
-cd ..
-
+sed -i 's/bionic/focal/g' /etc/apt/sources.list
+add-apt-repository universe
+add-apt-repository multiverse
+apt-get update
+apt-get install --no-install-recommends gstreamer1.0-tools gstreamer1.0-alsa \
+gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+gstreamer1.0-libav
+apt-get install --no-install-recommends libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev \
+libgstreamer-plugins-good1.0-dev \
+libgstreamer-plugins-bad1.0-dev
+apt install --no-install-recommends \
+gstreamer1.0-x \
+libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev \
+gstreamer1.0-plugins-bad \
+libgstreamer-plugins-bad1.0-dev \
+libsoup2.4-dev \
+libjson-glib-dev \
+libnice-dev \
+autoconf \
+libtool \
+gtk-doc-tools \
+libglib2.0-dev \
+gstreamer1.0-nice \
+libssl-dev \
+libreadline-dev \
+gstreamer1.0-plugins-ugly \
+libsrtp0-dev \
+libsrtp2-dev
+apt-get install --no-install-recommends libssl1.0-dev
+apt clean
+sed -i 's/focal/bionic/g' /etc/apt/sources.list
+apt update
